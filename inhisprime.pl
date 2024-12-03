d408108d7e27b356a162f9d56f5ef0846ecc5c31
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-03 17:42:32 +0000

    Update inhisprime.pl

--- inhisprime.pl
+++ inhisprime.pl
@@ -5,12 +5,6 @@ main
 mondays(D, S, X) :-
     findall(Day, (between(1, D, Day), (((Day + S - 2) mod 7) + 1) =:= 1), X).
 
--- provides month names and their lengths.
-mnd(M, 31) :- member(M, [1, 3, 5, 7, 8, 10, 12]).
-mnd(M, 30) :- member(M, [4, 6, 9, 11]).
-mnd(2, 28).
-mnd(2, 29).
-
 -- generates pairs of consecutive months list
 generator([[M1, D1, S1], [M2, D2, S2]]) :-
     between(1, 12, M1),
@@ -19,6 +13,14 @@ generator([[M1, D1, S1], [M2, D2, S2]]) :-
     mnd(M2, D2),
     between(1, 7, S1),
     S2 is (S1 + D1 - 1) mod 7 + 1.
+    
+-- provides month names and their lengths.
+mnd(M, 31) :- member(M, [1, 3, 5, 7, 8, 10, 12]).
+mnd(M, 30) :- member(M, [4, 6, 9, 11]).
+mnd(2, 28).
+mnd(2, 29).
+
+
 
 -- maps month numbers
 mn(1, "January").
