08df9f7ac3be28a72481176c8700d0fb85309723
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-10 06:00:03 +0000

    Update inhisprime.pl

--- inhisprime.pl
+++ inhisprime.pl
@@ -1,19 +1,21 @@
 main
   :- generator(X), selector(X), write(X).
 
-% calculates specific day properties in a month
-mondays(D, S, X) :-
-    findall(Day, (between(1, D, Day), ((Day + S - 2) mod 7) =:= 0), X).
-
 % generates pairs of consecutive months list
 generator([[M1, D1, S1], [M2, D2, S2]]) :-
     between(1, 12, M1),
-    M2 is (M1 mod 12) + 1,
     monthndays(M1, D1),
+    addWrap(12, M1, 1, M2),
     monthndays(M2, D2),
     between(1, 7, S1),
-    S2 is (S1 + D1 - 1) mod 7 + 1.
+    T is D1 mod 7,
+    addWrap(7, S1, T, S2).
     
+% Wrap-around addition with modulo logic for months and weekdays
+addWrap(Limit, P, Q, R) :-
+    T is P + Q,
+    (T > Limit -> R is T - Limit; R is T).
+
 % provides month names and their lengths.
 monthndays(M, 31) :- member(M, [1, 3, 5, 7, 8, 10, 12]).
 monthndays(M, 30) :- member(M, [4, 6, 9, 11]).
@@ -22,14 +24,17 @@ monthndays(2, 29).
 
 % fliters all conditions
 selector([[M1, D1, S1], [M2, D2, S2]]) :-
-    D1 = T1, D2 = U1,
-    monthlen(M1, T2), monthlen(M2, U2),
-    primeday(M1, T3), primeday(M2, U3),
-    mondaysinmonth(D1, S1, T4), mondaysinmonth(D2, S2, U4),
-    primesaturdays(D1, S1, T5), primesaturdays(D2, S2, U5),
-    T1 \= U1, T2 \= U2, T3 \= U3, T4 \= U4, T5 \= U5,
+    measure(M1, D1, S1, T1, T2, T3, T4, T5),
+    measure(M2, D2, S2, U1, U2, U3, U4, U5),
+    T1 =\= U1, T2 =\= U2, T3 =\= U3, T4 =\= U4, T5 =\= U5,
     prime(T1 + T2 + T3 + T4 + T5),
     prime(U1 + U2 + U3 + U4 + U5).
+
+measure(M, D, S, D, V2, V3, V4, V5)
+  :- monthlen(M, V2),
+     primes(D, V3),
+     mondays(D, S, TS), length(TS, V4),
+     primeSaturdays(D, S, US), length(US, V5).
 
 % maps month numbers
 monthnumbers(1, "January").
@@ -50,6 +55,32 @@ monthlen(M, Length) :-
     monthnumbers(M, Name),
     atom_length(Name, Length).
 
+primes(28, 9).
+primes(29, 10).
+primes(30, 10).
+primes(31, 11).
+
+mondays(D, S, TS)
+  :- T is 7 - S,
+     addWrap(7, T, 2, I),
+     fromToStep(I, D, 7, TS).
+
+saturdays(D, S, TS)
+  :- T is 7 - S,
+     addWrap(7, T, 0, I),
+     fromToStep(I, D, 7, TS).
+
+fromToStep(I, D, _, [])
+  :- I > D.
+fromToStep(I, D, S, [I |IS])
+  :- I =< D,
+     I1 is I + S,
+     fromToStep(I1, D, S, IS).
+
+primeSaturdays(D, S, US)
+  :- saturdays(D, S, TS),
+     intersection(TS, [2 ,3 ,5 ,7 ,11 ,13 ,17 ,19 ,23 ,29 ,31], US).
+
 % check if a number is prime
 prime(N)
   :- N > 1, \+ factorisable(2, N ).
@@ -61,18 +92,3 @@ factorisable(F, N)
   :- F*F =< N,
      F1 is F + 1,
      factorisable(F1, N).
-
-% finds all the prime days in a month
-primeday(D, X) :-
-    findall(Day, (between(1, D, Day), prime(Day)), Y),
-    length(Y, X).
-
-% finds all the prime saturdays in a month
-primesaturdays(D, S, X) :-
-    findall(Day, (between(1, D, Day), prime(Day), (Day + S - 1) mod 7 =:= 6), PS),
-    length(PS, X).
-
-% finds all the mondays in a month
-mondaysinmonth(D, S, X) :-
-    mondays(D, S, Y),
-    length(Y, X).
